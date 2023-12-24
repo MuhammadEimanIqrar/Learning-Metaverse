f2ef4746a387d3ddf1c0a484eba886994de2f84e
Muhammad Eiman Iqrar <meiman2485@gmail.com>
2023-12-24 19:46:11 +0500

    Plane Created in Metaverse

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 // Declaration of a new scene with Three.js
 const scene = new THREE.Scene();
-// scene.background = new THREE.Color(0xbfd1e5);
+scene.background = new THREE.Color(0xbfd1e5);
 
 // Camera & Renderer Configuration
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -16,12 +16,20 @@ const direction_light = new THREE.DirectionalLight(0xffffff, 1);
 ambient_light.add(direction_light);
 scene.add(ambient_light);
 
+// Setting up the plane in the Mertaverse
+const geometry_space = new THREE.BoxGeometry(100, 0.2, 50);
+const material_space = new THREE.MeshPhongMaterial({ color: 0xffffff });
+const space = new THREE.Mesh(geometry_space, material_space);
+scene.add(space);
+
 // Cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 camera.position.z = 5;
+
+camera.position.set(10, 5, 40);
 
 function animate() {
     cube.rotation.x += 0.05;
